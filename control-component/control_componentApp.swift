51f605827a7c06d2f52cc0159f65f5577ea8aee6
David Sloane <david@cambrean.com>
2024-01-19 19:59:13 +0000

    Initial Commit

---
+++ control-component/control_componentApp.swift
@@ -0,0 +1,17 @@
+//
+//  control_componentApp.swift
+//  control-component
+//
+//  Created by David Sloane on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Volume_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Slider()
+        }
+    }
+}
